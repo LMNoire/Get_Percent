7c72090040a3309c417d95d706487feae7d50934
LMNoire <149476454+LMNoire@users.noreply.github.com>
2023-11-19 19:47:21 +0100

    Start coding functions to calculate percentage

---
+++ M/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+/*Function convert a percent into a number*/
+function convertPercentToNumber (float $percent1, float $num1): float {
+    $res1 = ($percent1*$num1)/100 ;
+    return $res1 ;
+}
+
+/*Function convert a number into a percent*/
+function convertNumberToPercent (float $num2, float $int2): float {
+    $res2 = ($num2*100)/$int2 ;
+    return $res2 ;
+}
+
+/*Function adding a percentage*/
+
+/*Function substracte a percentage*/
+
+/*Function percentage variation*/
+
+?>
+
